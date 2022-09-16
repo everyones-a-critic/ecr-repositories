1e0f225621e0a51dea9d616997a0d51a43bf6257
Daniel Long <24.daniel.long@gmail.com>
2022-09-16 09:57:29 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "communities-service" {
+  name                 = "eac-communities-service"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "communities_service_ecr_id" {
+  value       = aws_ecr_repository.communities-service.id
+}
