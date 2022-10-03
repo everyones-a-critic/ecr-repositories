7ad4537623889dfe6f8b3f1ea046a7d87bb9a105
Daniel Long <24.daniel.long@gmail.com>
2022-10-03 15:32:26 -0500

    Adds an output for ratings service repo url

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "communities_service_repository_url" {
   value       = aws_ecr_repository.communities-service.repository_url
 }
+
+output "ratings_service_repository_url" {
+  value       = aws_ecr_repository.ratings-service.repository_url
+}
