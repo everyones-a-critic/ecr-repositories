8ca40dd7a39f3c6b0705d925f51884e666b2cded
Daniel Long <24.daniel.long@gmail.com>
2022-10-03 15:31:26 -0500

    Adds ratings service ECR repo

--- main.tf
+++ main.tf
@@ -43,3 +43,36 @@ resource "aws_ecr_lifecycle_policy" "communities-service" {
 }
 EOF
 }
+
+resource "aws_ecr_repository" "ratings-service" {
+  name                 = "eac-ratings-service"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "ratings-service" {
+  repository = aws_ecr_repository.ratings-service.id
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
